665f389be66bdb53c70c95aa77469ee7af1a082b
philippolidan <cxpolidan@gmail.com>
2018-09-05 15:20:24 +0800

    database migration

---
+++ database/migrations/2018_09_05_064907_create_authentications_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAuthenticationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('authentications', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->integer('order_id');
+            $table->string('icon');
+            $table->integer('parent_id')->default(0);
+            $table->string('path');
+            $table->string('type');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('authentications');
+    }
+}
---
+++ database/migrations/2018_09_05_070633_create_roles_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateRolesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('roles', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('role');
+            $table->string('description');
+            $table->integer('active');
+            $table->integer('superuser');
+            $table->string('default_page');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('roles');
+    }
+}
