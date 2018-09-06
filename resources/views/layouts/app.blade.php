d51260988755352d0ccb1b2a2e64567fd7411775
Mark Dherrick Cuevas <42988318+dherrickcuevas@users.noreply.github.com>
2018-09-06 15:41:51 +0800

    Login Ver 1.1

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,7 +6,6 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 
 	<link rel="stylesheet" type="text/css" href="{{asset('css/app.css')}}">
-	<link href="https://fonts.googleapis.com/css?family=Poppins" rel="stylesheet">
 
 	<script type="text/javascript" src="{{asset('js/solid.min.js')}}"></script>
 	<script type="text/javascript" src="{{asset('js/animate.min.js')}}"></script>
--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -16,28 +16,68 @@
 
 @media(min-width: 990px){
 	#login_form{
-		padding: 50px;
+		padding: 0px;
 	}
+}
+
+.carousel-background{
+	height: 100vh;
+	filter: blur(2px);
+	-webkit-filter: rgba(0,0,0,1);
 }
 
 </style>
 
 <div class="fluid w-100">
 	<div class="row" style="margin-left: 0px; margin-right: 0px;">
-		<div id="design" class="col-lg-8 col-md-12 col-xs-12 col-sm-12 bg-dark" style="height: 100vh;">
+
+		<div id="design" class="col-lg-8 col-md-12 col-xs-12 col-sm-12 bg-light" style="height: 100vh; padding-right: 0; padding-left: 0;">
+			<div id="carouselExampleFade" class="carousel slide carousel-fade" data-ride="carousel" style="height: 100vh">
+				<div class="carousel-inner">
+
+					<div class="carousel-item active">
+						<img class="img-responsive carousel-background" src="{{asset('img/carousel/laptop-3196481_1920.jpg')}}">
+						<div class="carousel-caption text-white" style="text-align: right;">
+							<h1>Time is Money</h1>
+							<h6>...</h6>
+						</div>
+					</div>
+
+					<div class="carousel-item">
+						<img class="img-responsive carousel-background" src="{{asset('img/carousel/laptop-3196481_1920.jpg')}}">
+						<div class="carousel-caption text-white" style="text-align: right;">
+							<h1>Convinient</h1>
+							<h6>...</h6>
+						</div>
+					</div>
+
+					<div class="carousel-item">
+						<img class="img-responsive carousel-background" src="{{asset('img/carousel/laptop-3196481_1920.jpg')}}">
+						<div class="carousel-caption text-white" style="text-align: right;">
+							<h1>Reliable</h1>
+							<h6>...</h6>
+						</div>
+					</div>
+
+					{{-- <div class="carousel-item">
+						<img class="carousel-background d-block" src="" alt="">
+					</div>
+
+					<div class="carousel-item">
+						<img class="carousel-background d-block" src="" alt="">
+					</div> --}}
+
+				</div>
+			</div>
 		</div>
 
 		<div id="login" class="col-lg-4 col-md-12 col-xs-12 col-sm-12 bg-white p-50" style="height: 100vh; padding: 50px; ">
 			<div class="row mt-5 mb-5">
 				<div class="col-lg-12">
 
-					
-
 					<form id="login_form" method="POST">
 
-						<img src="{{asset('img/logo/alpax_logo.svg')}}" style="height: 8vh; margin-top: 20px; margin-bottom: 20px;">
-
-					<h4>ALPAX Software Solutions</h4>
+						<h2>Ez Pay Plus</h2>
 
 						<hr>
 
@@ -54,17 +94,25 @@
 						<hr>
 
 						<div class="form-group">
-							<button class="btn btn-primary btn-block">LOG IN</button>
+							<button class="btn bg-orange btn-block">LOG IN</button>
 						</div>
-
 						
 					</form>
-
-					
 
 				</div>
 			</div>
 
+			<div class="mt-5 mb-5" style="bottom: 0; position: absolute; padding: 10px">
+				<div class="row">
+					<br><img src="{{asset('img/logo/alpax_logo.svg')}}" style="height: 25px; margin-right: 5px; margin-left: 5px"><span class="text-muted">ALPAX Software Solutions</span>
+				</div>
+			</div>
+
 		</div>
+
 	</div>
+
+	<script type="text/javascript">
+		
+	</script>
 	@endsection
